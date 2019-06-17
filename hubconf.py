6b8152f67f0a2e9eb42b042819acdae5e6f5de11
Ross Taylor <rj-taylor@live.co.uk>
2019-06-17 18:26:10 +0100

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,4 +1,4 @@
-dependencies = ['scipy', 'torchvision', 'torch', 'math']
+dependencies = ['scipy', 'torchvision', 'torch']
 
 from gen_efficientnet import efficientnet_b0
 from gen_efficientnet import mobilenetv3_100
